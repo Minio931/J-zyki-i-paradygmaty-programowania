dca2c6a4245019202379024f0e2f55fb15bed70d
Chmielowski Dominik <w65529@student.wsiz.edu.pl>
2023-10-18 13:19:00 +0200

    lab 1

---
+++ lab-1/rodzina.pl
@@ -0,0 +1,32 @@
+rodzic(zofia,marcin).
+rodzic(andrzej,marcin).
+rodzic(andrzej,kasia).
+rodzic(marcin,ania).
+rodzic(marcin,krzys).
+rodzic(krzys,mikolaj).
+
+potomek(Y,X):-
+    rodzic(X,Y).
+
+matka(X,Y):-
+    rodzic(X,Y),
+    kobieta(X).
+dziadek(X,Z):-
+    rodzic(X,Y),
+    rodzic(Y,Z).
+
+siostra(X,Y):-
+    rodzic(Z,X),
+    rodzic(Z,Y),
+    kobieta(X),
+    X\=Y.
+
+kobieta(zofia).
+kobieta(kasia).
+kobieta(ania).
+
+mezczyzna(andrzej).
+mezczyzna(marcin).
+mezczyzna(krzys).
+mezczyzna(mikolaj).
+
